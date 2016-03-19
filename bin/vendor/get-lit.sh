b047fca1d21a0a8b4e56d490856a243d42939f66
L-Elf Karlstein <lelfjior@gmail.com>
2016-03-19 04:28:43 -0500

    ELF_HOST=luvit installation no longer requires make, git, or curl

---
+++ bin/vendor/get-lit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+LUVI_VERSION=2.6.1
+LIT_VERSION=3.1.1
+
+LUVI_ARCH=`uname -s`_`uname -m`
+LUVI_URL="https://github.com/luvit/luvi/releases/download/v$LUVI_VERSION/luvi-regular-$LUVI_ARCH"
+LIT_URL="https://lit.luvit.io/packages/luvit/lit/v$LIT_VERSION.zip"
+
+# Download Files
+echo "Downloading $LUVI_URL to luvi"
+curl -L $LUVI_URL > luvi
+chmod +x luvi
+echo "Downloading $LIT_URL to lit.zip"
+curl -L $LIT_URL > lit.zip
+
+# Create lit using lit
+./luvi lit.zip -- make lit.zip
+
+# Cleanup
+rm -f lit.zip
